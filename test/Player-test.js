314c5f17fc3e83c7404a959b7c42aa743b9935e5
Spencer Herms <slherms@gmail.com>
2017-12-02 22:37:46 -0700

    adds player unit testing

--- test/Player-test.js
+++ test/Player-test.js
@@ -1,11 +1,37 @@
 const chai = require('chai');
-const expect = chai.expect;
+const assert = chai.assert;
 const Player = require('../lib/Player');
+
+beforeEach( () => {
+  player = new Player(300, 300, 30, 30);
+});
 
 describe('Player unit testing', () => {
   
-  it('should', () => {
-    
-      });
-      
+  it('should instantiate a player', () => {
+    console.log(player);
+      assert.isObject(player); 
+  });
+
+  it('should take arguments for x, y, h and w', () => {
+    assert.equal(player.x, 300);
+    assert.equal(player.y, 300);
+    assert.equal(player.h, 30);
+    assert.equal(player.w, 30);
+  });
+
+  it('should have a default speed of 5', () => {
+    assert.equal(player.speed, 5);
+  });
+
+  it('should be able to take an argument for speed', () => {
+    player = new Player(300, 300, 30, 30, 10);
+
+    assert.equal(player.speed, 10);
+  });
+
+  it('should have a default of isOnBoard set to true ', () => {
+    assert.equal(player.isOnBoard, true);
+  });
+
 });
